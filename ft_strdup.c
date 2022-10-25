2a29084e770788defa9bd3205676797f412e563c
Mattis Courtin <mcourtin@student.s19.be>
2022-10-25 10:33:39 +0200

    libft

---
+++ ft_strdup.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mcourtin <mcourtin@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/06 12:40:39 by mcourtin          #+#    #+#             */
+/*   Updated: 2022/10/07 11:05:53 by mcourtin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strdup(const char *s1)
+{
+	size_t	i;
+	char	*dst;
+
+	i = 0;
+	dst = malloc(sizeof(char) * ft_strlen(s1) + 1);
+	if (!dst)
+		return (0);
+	while (s1[i])
+	{
+		dst[i] = s1[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (dst);
+}
